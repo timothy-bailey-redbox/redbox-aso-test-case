cd3a0693821c6b2671278c38c38ab59bf7c0ad96
Timothy Bailey <timothy.bailey@redboxmobile.com>
2024-01-16 17:19:12 +0000

    Get single dashboard

--- netlify/functions/dashboard.ts
+++ netlify/functions/dashboard.ts
@@ -1,5 +1,10 @@
 import { type Config } from "@netlify/functions";
+import { uiDb } from "netlify/lib/db";
+import { dashboardDBToAPI } from "netlify/lib/dto/dashboard";
 import functionHandler from "netlify/lib/handler";
+import { type DashboardDB } from "types/dashboard";
+import { StatusSchema } from "types/generic";
+import { type WidgetDB } from "types/widget";
 
 export default functionHandler({
     secure: true,
@@ -11,7 +16,32 @@ export default functionHandler({
                 user,
             });
 
-            return {};
+            const { dashboardId } = context.params;
+
+            const dashboard = await uiDb.transaction(async (queryFn) => {
+                const [dashboard] = await queryFn<DashboardDB>(
+                    `SELECT d.*
+                    FROM dashboards d
+                    INNER JOIN teams t ON t.id = d."teamId"
+                    WHERE "id" = $1
+                    AND $2::text = ANY(t.users)
+                    AND d."status" = $3
+                    AND t."status" = $3`,
+                    [dashboardId, user.email, StatusSchema.Values.ACTIVE],
+                );
+                const widgets = await queryFn<WidgetDB>(
+                    `SELECT *
+                    FROM widgets
+                    WHERE "dashboardId" = $1`,
+                    [dashboardId],
+                );
+
+                return dashboardDBToAPI(dashboard, widgets);
+            });
+
+            return {
+                body: JSON.stringify(dashboard),
+            };
         },
     },
 });
---
+++ netlify/lib/dto/dashboard.ts
@@ -0,0 +1,45 @@
+import { type DashboardAPI, type DashboardDB } from "types/dashboard";
+import { type WidgetAPI, type WidgetDB } from "types/widget";
+import { HTTPResponseError } from "../handler";
+
+export function dashboardDBToAPI(dashboard?: DashboardDB, widgets: WidgetDB[] = []): DashboardAPI {
+    if (!dashboard) {
+        throw new HTTPResponseError(404, "Missing dashboard data to convert to response");
+    }
+    return {
+        id: dashboard.id,
+
+        name: dashboard.name,
+        description: dashboard.description,
+        status: dashboard.status,
+        teamId: dashboard.teamId,
+
+        appId: dashboard.appId,
+        appType: dashboard.appType,
+        comparisonAppIds: dashboard.comparisonAppIds,
+
+        keywords: dashboard.keywords,
+
+        updatedAt: dashboard.updatedAt,
+        createdAt: dashboard.createdAt,
+
+        widgets: widgets.map(
+            (widget) =>
+                ({
+                    id: widget.id,
+                    type: widget.type,
+                    title: widget.title,
+                    description: widget.description,
+                    width: widget.width,
+                    height: widget.height,
+                    x: widget.x,
+                    y: widget.y,
+                    dataSource: widget.dataSource,
+                    dataType: widget.dataType,
+                    axis1: widget.axis1,
+                    axis2: widget.axis2,
+                    axis3: widget.axis3,
+                }) satisfies WidgetAPI,
+        ),
+    } satisfies DashboardAPI;
+}
