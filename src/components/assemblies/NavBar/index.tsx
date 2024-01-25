e8da089c5805a7ed355b1deeed1551059d709fc6
Timothy <timothy.bailey@redboxmobile.com>
2024-01-25 09:12:57 +0000

    Extracted the page layout into a component and made nav and background less responsible for each other

--- src/components/assemblies/NavBar/index.tsx
+++ src/components/assemblies/NavBar/index.tsx
@@ -1,13 +1,14 @@
-import Card from "~/components/basic/Card";
+import Icons from "~/components/basic/Icons";
 import TextInput from "../../basic/inputs/TextInput";
 import styles from "./navbar.module.css";
-import Icons from "~/components/basic/Icons";
 
 export default function NavBar() {
     return (
-        <Card className={styles.navbar}>
-            <Icons.Redbox height={100} width={132} />
-            <TextInput placeHolder="Search..."></TextInput>
-        </Card>
+        <div className={styles.navbar}>
+            <div className={styles.wrapper}>
+                <Icons.Redbox height={100} width={132} />
+                <TextInput placeHolder="Search..."></TextInput>
+            </div>
+        </div>
     );
 }
