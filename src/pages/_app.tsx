581618e0cae39040846525e4466c46ddcc3df115
Timothy <timothy.bailey@redboxmobile.com>
2024-02-12 10:09:35 +0000

    Removed more junk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import Head from "next/head";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { minutes } from "~/lib/time";
 
 import "~/styles/globals.css";
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            refetchOnMount: false,
-            staleTime: minutes(10),
-        },
-    },
-});
-
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
-        <QueryClientProvider client={queryClient}>
+        <>
             <Head>
                 <title>ASO Dashboard - Redbox Mobile</title>
                 <meta name="description" content="App Store Optimisation dashboards by Redbox Mobile" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Component {...pageProps} />
-        </QueryClientProvider>
+        </>
     );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
             <Head />
             <body>
                 <Main />
-                <div id="identity"></div>
                 <NextScript />
             </body>
         </Html>
